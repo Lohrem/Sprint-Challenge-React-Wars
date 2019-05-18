968651fb3e91fa8171fc67fd15b33600a05ff86d
Lohrem <30306978+Lohrem@users.noreply.github.com>
2019-05-18 14:26:04 -0400

    minimal styling added, MVP complete

--- starwars/src/components/StarWarsList.js
+++ starwars/src/components/StarWarsList.js
@@ -8,7 +8,7 @@ const StarWarsList = props => {
     <div className="char-card">
       <h1 className="char-name">{props.charName}</h1>
       <p className="char-info">
-        Born on {props.birth_year} and is {props.height}cm tall and weighs {props.mass} kg. {props.charName}'s skin tone is {props.skin_color}
+        Born on {props.birth_year} and is {props.height}cm tall and weighs {props.mass} kg. {props.charName}'s skin tone is {props.skin_color}.
       </p>
     </div>
   )
